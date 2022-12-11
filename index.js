c7fca2a28a2c49d6f27e1512704380ab0dfbc440
erynder-z <stfn.bgr@gmail.com>
2022-12-11 21:36:28 +0100

    Initialize middleware

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 const env = require('dotenv').config();
 const openaiRoutes = require('./routes/openaiRoutes');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', openaiRoutes);
 
